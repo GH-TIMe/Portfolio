867b37a159e7db089087d75858e7677fb0196cdc
TIMe <timhaha@yandex.ru>
2021-01-02 16:45:29 +0300

    removed works folder, changed technology stack

--- works/recognition/models/script.js
+++
@@ -1,7 +0,0 @@
-(async function() {
-
-    await faceapi.nets.ssdMobilenetv1.loadFromDisk('./models/');
-    
-    const input = document.getElementById('myImage');
-    let fullFaceDescriptions = await faceapi.detectAllFaces(input).withFaceLandmarks().withFaceDescriptors();
-}());
